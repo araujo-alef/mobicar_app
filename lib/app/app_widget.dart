0013569d6c824d7a479d6c888f40ea4616289604
Alef Deuan Araujo da Silva <alefaraujo.dev@gmail.com>
2021-12-15 19:24:28 -0300

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobicar_app/app/designsystem/styles/app_theme_data.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeData.themeData,
+    ).modular();
+  }
+}
